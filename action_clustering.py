e65a5640d007eeefca87bbcc4ad1d8460efd16ca
Arunima Arunima <168588268+aruuniima@users.noreply.github.com>
2025-11-11 18:36:06 +1100

    improved clarity, put comments
    Should work for getting data for fig 2,3

--- action_clustering.py
+++ action_clustering.py
@@ -1,3 +1,4 @@
+#imports
 import numpy as np
 import random
 from tqdm import tqdm
@@ -10,19 +11,7 @@ from scipy.spatial.distance import pdist, squareform
 import pickle
 import os
 
-which_j = 0
-output_file = '/g/data/jh2/ax8338/project2/save_data/JR_60pc_allbins.h5'
-logfile = "/g/data/jh2/ax8338/project2/save_data/completed_timesteps_JR.txt"
-
-sep_threshold=60
-# birth_bins = np.array((1000,1200,1400,1600,1800,2000,5000))
-birth_bins = np.array((0.15,0.5,2,5,10,50,100,200,300,400,500,600,700,800,900,1000,1200,1400,1600,1800,2000,5000))
-# np.logspace(-0.8,3,8)
-
-birth_window = 1 #Myr
-delta_t_range = np.arange(1, 457, 1)
-n_bins = len(birth_bins)-1
-
+#function 
 def convert_to_cartesian(phi_R_z):
     """
     Convert (phi, R, z) to Cartesian coordinates (x, y, z).
@@ -42,12 +31,25 @@ def convert_to_cartesian(phi_R_z):
     
     return np.column_stack((x, y, z))
 
+# which action component, 0 is radial, 1 is azimuthal and 2 is vertical action
+which_j = 0
+
+output_file = 'insert_output_file_path_here.h5'
+logfile = "insert_log_file_path_here.txt"    #to keep track of which timesteps have been done
+
+sep_threshold=30 # the threshold distance to separate unbound stars - we only want to analyse these. 30 pc comes from the Fig 1 of the paper
+
+birth_bins = np.array((0.15,0.5,2,5,10,50,100,200,300,400,500,600,700,800,900,1000,1200,1400,1600,1800,2000,5000))   #bins of birth separation between two stars - these are the values used in Fig.2,3
+
+birth_window = 1 #Myr
+delta_t_range = np.arange(1, 457, 1)
+n_bins = len(birth_bins)-1
+
 print('loading data')
-#load data # can try to directly load filtered data - filt_C, filt_J, A
-A=np.load('/g/data/jh2/ax8338/project2/A.npy')
-J=np.load('/g/data/jh2/ax8338/project2/filt_J.npz')
-#filtered data saved as JR,Jz,Jphi
-C=np.load('/g/data/jh2/ax8338/project2/C.npy')   #in cylindrical coordinates
+#load data. CHANGE PATH HERE
+A=np.load('path_to/A.npy')
+J=np.load('path_to/filt_J.npz') #filtered data saved as JR,Jz,Jphi
+C=np.load('path_to/C.npy')   #in cylindrical coordinates
 
 if which_j==0:
     filt_J = J['JR']
@@ -70,32 +72,24 @@ bin_pair_indices = defaultdict(list)  # {bin_id: [(t0, i1, i2), ...]}
 
 # Loop over all snapshots to get all pairwise birth distances
 for t0 in tqdm(range(C_cart.shape[0]),desc='looping to get pairwise birth distances in each bin'):
-    born_now = np.where((A[t0, :] >= 0) & (A[t0, :] < birth_window))[0]
+    born_now = np.where((A[t0, :] >= 0) & (A[t0, :] < birth_window))[0]   #get stars born in the last [birth_window] Myr
+    #if not even one pair found, skip.
     if len(born_now) < 2: continue
-    if (C[t0,born_now,1]>18000).any():
-        print(f'{(C[t0,born_now,1]>18000).sum()} stars outside range')
     coords_now = C_cart[t0, born_now, :]  # shape (N, 3)
     dist_matrix = squareform(pdist(coords_now))  # (N, N)
     triu_inds = np.triu_indices(len(born_now), k=1)
-    dists = dist_matrix[triu_inds]
-    bin_indices = np.digitize(dists, birth_bins) - 1
+    dists = dist_matrix[triu_inds] #getting distances between all pairs
+    bin_indices = np.digitize(dists, birth_bins) - 1        #putting distances in the birth_bins defined earlier
     for idx, b in enumerate(bin_indices):
         if 0 <= b < len(birth_bins) - 1:
             i1 = born_now[triu_inds[0][idx]]
             i2 = born_now[triu_inds[1][idx]]
-            bin_pair_indices[b].append((t0, i1, i2))
+            bin_pair_indices[b].append((t0, i1, i2))        #stores snapshot number and the indices of the pair of star that fall in each birth_bin
 
 ##creating hdf5 file
 if not os.path.exists(output_file):
     with h5py.File(output_file, "w") as f:
-        f.create_dataset("bin_centres", data=0.5 * (birth_bins[:-1] + birth_bins[1:]))
-        f.create_dataset("birth_bin_final_diff", shape=(len(delta_t_range), n_bins),dtype='f8', compression="gzip", fillvalue=np.nan)
-        f.create_dataset("birth_bin_relchange", shape=(len(delta_t_range), n_bins),
-                         dtype='f8', compression="gzip", fillvalue=np.nan)
-        f.create_dataset("birth_bin_abschange", shape=(len(delta_t_range), n_bins),
-                         dtype='f8', compression="gzip", fillvalue=np.nan)
-        # NEW: For pairs that drifted to >30pc separation at 100 Myr
-        f.create_dataset("birth_bin_final_diff_gt30pc", shape=(len(delta_t_range), n_bins), dtype='f8', compression="gzip", fillvalue=np.nan)
+          # For pairs that drifted to >30pc separation at 100 Myr, absolute and relative change in action differences
         f.create_dataset("birth_bin_abschange_gt30pc", shape=(len(delta_t_range), n_bins), dtype='f8', compression="gzip", fillvalue=np.nan)
         f.create_dataset("birth_bin_relchange_gt30pc", shape=(len(delta_t_range), n_bins), dtype='f8', compression="gzip", fillvalue=np.nan)
 
@@ -107,14 +101,9 @@ else:
     completed = set()
 
 with h5py.File(output_file, "a") as f:
-    final_diff_ds = f["birth_bin_final_diff"]
-    abs_change_ds = f["birth_bin_abschange"]
-    rel_change_ds = f["birth_bin_relchange"]
 
-    if "birth_bin_final_diff_gt30pc" in f:
-        gt30pc_final_ds = f["birth_bin_final_diff_gt30pc"]
-        gt30pc_abs_ds = f["birth_bin_abschange_gt30pc"]
-        gt30pc_rel_ds = f["birth_bin_relchange_gt30pc"]
+    gt30pc_abs_ds = f["birth_bin_abschange_gt30pc"]
+    gt30pc_rel_ds = f["birth_bin_relchange_gt30pc"]
 
     for i, delta_t in enumerate(tqdm(delta_t_range, desc="Looping over Δt")):
         if i in completed:
@@ -123,42 +112,31 @@ with h5py.File(output_file, "a") as f:
         
         print(f"\n>>> Starting Δt={delta_t} (index {i})")
 
-            
-        final_row = []
-        abs_row = []
-        rel_row = []
-        
 
-        gt30pc_final_row=[]
         gt30pc_abs_row=[]
         gt30pc_rel_row=[]
             
         for b in range(n_bins):
             pairs = np.array(bin_pair_indices[b])
-            if len(pairs) == 0:
-                final_row.append(np.nan)
-                abs_row.append(np.nan)
-                rel_row.append(np.nan)
-                continue
 
+            #getting the indices and snapshots of the pair of stars
             t0s, i1s, i2s = pairs[:, 0], pairs[:, 1], pairs[:, 2]
-            valid = t0s + delta_t < filt_J.shape[0]
+            valid = t0s + delta_t < filt_J.shape[0]  #only choosing where t0s+delta_t is within our simulation time
             t0s = t0s[valid]
             i1s = i1s[valid]
             i2s = i2s[valid]
 
-            J0_1 = filt_J[t0s, i1s]
-            J0_2 = filt_J[t0s, i2s]
-            Jt_1 = filt_J[t0s + delta_t, i1s]
-            Jt_2 = filt_J[t0s + delta_t, i2s]
+            J0_1 = filt_J[t0s, i1s] #actiion of star 1 at earlier snapshot
+            J0_2 = filt_J[t0s, i2s] #action of star 2 at earlier snapshot
+            Jt_1 = filt_J[t0s + delta_t, i1s] #action of star 1 at later snapshot 
+            Jt_2 = filt_J[t0s + delta_t, i2s]    #action of star 2 at later snapshot
 
-            deltaJ0 = np.abs(J0_1 - J0_2)
-            deltaJt = np.abs(Jt_1 - Jt_2)
-            abs_change = np.abs(deltaJt - deltaJ0)
-            rel_change = abs_change / np.sqrt(Jt_1 * Jt_2)
+            deltaJ0 = np.abs(J0_1 - J0_2)    #absolute difference in action of stars at previous snapshot
+            deltaJt = np.abs(Jt_1 - Jt_2)    #absolute difference in actions of stars at later snapshot
+            abs_change = np.abs(deltaJt - deltaJ0)    #absolute change in action differences
+            rel_change = abs_change / np.sqrt(Jt_1 * Jt_2)    #relative change in action difference
 
-            print(f'{len(rel_change)}- total sample in this bin {b}')
-            #saving stars that are >30pc apart after 100 Myr
+            #saving stars that are >30pc apart after 100 Myr (to select unbound stars)
             new_valid_mask = (t0s+100)<filt_J.shape[0]
             t0s=t0s[new_valid_mask]
             i1s = i1s[new_valid_mask]
@@ -172,18 +150,15 @@ with h5py.File(output_file, "a") as f:
             sep = np.linalg.norm(pos1 - pos2, axis=1)  # just the distance between them at the later time so for later bins, might include everything
             mask_gt30 = sep >sep_threshold
             if np.sum(mask_gt30) ==0:
-                gt30pc_final_row.append(np.nan)
                 gt30pc_abs_row.append(np.nan)
                 gt30pc_rel_row.append(np.nan)
             else:
-                gt30pc_final_row.append(np.nanmedian(deltaJt[mask_gt30]))
                 gt30pc_abs_row.append(np.nanmedian(abs_change[mask_gt30]))
                 gt30pc_rel_row.append(np.nanmedian(rel_change[mask_gt30]))
                 print(f'{np.sum(mask_gt30)}- >30pc sample in this bin {b}')
 
 
-        
-        gt30pc_final_ds[i,:] = gt30pc_final_row
+        #saving everything        
         gt30pc_abs_ds[i,:] = gt30pc_abs_row
         gt30pc_rel_ds[i,:] = gt30pc_rel_row
         
