203318b7d35a2046864f0a6dfde28be75a62f172
Arunima Arunima <168588268+aruuniima@users.noreply.github.com>
2025-11-20 15:45:41 +1100

    Refactor file paths and add author details
    Updated file paths and added author information.

--- plots/Fig1.py
+++ plots/Fig1.py
@@ -1,8 +1,24 @@
-# Figure 1 – 5th nearest-neighbour distance distribution for coeval stars
-# can get action.h5 file from the following website and change the file paths here accordingly ('stellar-actions-I/data/actions.h5' in following code)
-# https://www.mso.anu.edu.au/~arunima/stellar-actions-I-data/
+"""
+ Figure 1 – 5th nearest-neighbour distance distribution for coeval stars
+ can get action.h5 file from the following website and change the file paths here accordingly ('stellar-actions-I/data/actions.h5' in following code)
+ https://www.mso.anu.edu.au/~arunima/stellar-actions-I-data/
+Change paths etc at the user configuration block.
 
-#imports
+Author: Arunima
+Date: 20–11–2025
+"""
+
+# ======================================================================
+# User CONFIGURATION - CHANGE PATH HERE
+# ======================================================================
+# ===================================================================================
+action_file = 'stellar-actions-I/data/actions.h5'
+output_fig_path = 'PATH_TO/fig1.pdf'
+ages_to_include = [2,5, 10, 100,200,300, 400] #Myr
+
+####### =========================
+# IMPORTS
+# =========================
 import matplotlib.pyplot as plt
 import numpy as np
 import h5py
@@ -10,8 +26,10 @@ from sklearn.neighbors import KDTree
 import seaborn as sns
 from tqdm import tqdm
 
-# ------------------------------------------------------------
-# Helper function: cylindrical to Cartesian coordinates
+# ======================================================================
+# HELPER FUNCTIONS
+# ======================================================================
+# cylindrical to Cartesian coordinates
 def cylind_to_cart(c_array):
       """Convert cylindrical (phi,R, z) to Cartesian (x, y, z)."""
     x = c_array[:,1] * np.cos(c_array[:,0])
@@ -34,15 +52,13 @@ plt.rc('legend', fontsize=15)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 # -------------------------------------------------------------------------------------------------------------------------
-#CHANGE PATH HERE!!!!!
 # Read HDF5
-with h5py.File('stellar-actions-I/data/actions.h5', 'r') as f:
+with h5py.File(action_file, 'r') as f:
     C = f['coordinates'][:] # Cylindrical coordinates (phi,R,z)
     A = f['age'][:]  # Stellar ages (Myr)
 
 # ------------------------------------------------------------
 # getting 5th nearest neighbour distance for coeval stars at different ages
-ages_to_include = [2,5, 10, 100,200,300, 400] #Myr
 age_distance_dict = {t: [] for t in ages_to_include}  # Dictionary to store distances per age group
 
 # Loop over all snapshots
@@ -72,8 +88,7 @@ plt.xscale('linear')
 plt.axvline(x=1.5,c='r',linestyle='--',alpha=0.8)
 plt.tight_layout()
 plt.minorticks_on()
-# CHANGE PATH HERE
-plt.savefig('PATH_TO/fig1.pdf')
+plt.savefig(output_fig_path)
 plt.show()
 
 
